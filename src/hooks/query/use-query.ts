775178adc11da2a8ca0eac4ba0c6007c6a5ce9a7
Pete <pete@manageable.works>
2024-05-14 11:18:31 +0100

    feat: create custom useQuery hook

---
+++ src/hooks/query/use-query.ts
@@ -0,0 +1,28 @@
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useCallback } from 'react'
+
+export function useQuery() {
+  const pathname = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const createQueryString = useCallback(
+    (name: string, value: string | number) => {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set(name, String(value))
+
+      return params.toString()
+    },
+    [searchParams],
+  )
+
+  function set(name: string, value: string | number) {
+    router.push(pathname + '?' + createQueryString(name, String(value)))
+  }
+
+  function clear() {
+    router.push(pathname)
+  }
+
+  return { set, clear }
+}
