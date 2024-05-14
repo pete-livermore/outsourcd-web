4d3f079b040840b91866d77fca16936068a01cda
Pete <pete@manageable.works>
2024-05-14 11:21:52 +0100

    feat: create job detail component

--- src/app/(auth)/talent/jobs/_components/job-detail.tsx
+++ src/app/(auth)/talent/jobs/_components/job-detail.tsx
@@ -1,19 +1,63 @@
+'use client'
 import { Banknote, Calendar } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { cn } from '@/utils/styles'
+import { Spinner } from '@/components/ui/spinner'
 
+import { Job } from '../page'
 import { CompanyInfo } from '.'
 
-export function JobDetail({ className }: { className?: string }) {
+interface JobDetailProps {
+  id: number
+}
+
+interface ApiError {
+  message: string
+}
+
+export function JobDetail({ id }: JobDetailProps) {
+  const [job, setJob] = useState<Job | undefined>()
+  const [error, setError] = useState<ApiError | undefined>()
+  const isLoading = !job && !error
+
+  useEffect(() => {
+    async function setJobData() {
+      const res = await fetch(`/api/jobs/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (res.ok) {
+        const data = await res.json()
+        setJob(data.job)
+      } else {
+        setError({ message: 'error' })
+      }
+    }
+
+    setJobData()
+  }, [id])
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (!job) {
+    return null
+  }
+
+  const { title, company, description } = job
+
   return (
-    <Card className={cn(className)}>
+    <Card>
       <CardHeader>
-        <CompanyInfo name='Company A' />
+        <CompanyInfo name={company.name} />
         <div className='space-y-4'>
-          <CardTitle className='mt-4'>Job Title</CardTitle>
+          <CardTitle className='mt-4'>{title}</CardTitle>
           <div className='mt-4 flex items-center gap-x-4'>
             <div className='flex items-center gap-x-1 text-xs'>
               <Calendar />
@@ -30,7 +74,7 @@ export function JobDetail({ className }: { className?: string }) {
         </div>
       </CardHeader>
       <CardContent>
-        <p className='py-4'>Here is a job description</p>
+        <p className='py-4'>{description}</p>
         <Button variant='default'>Apply</Button>
       </CardContent>
     </Card>
