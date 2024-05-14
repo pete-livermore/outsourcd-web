4a3ad5d55ccae75d29774d55afede91791f1cc99
Pete <pete@manageable.works>
2024-05-14 11:20:17 +0100

    refactor: add dialog trigger component

--- src/app/(auth)/talent/jobs/_components/jobs-list.tsx
+++ src/app/(auth)/talent/jobs/_components/jobs-list.tsx
@@ -1,27 +1,20 @@
+import { DialogTrigger } from '@/components/ui/dialog'
+
 import { Job } from '../page'
 import { JobsListItem } from './jobs-list-item'
 
 export async function JobsList({ jobs }: { jobs: Job[] }) {
-  // const createQueryString = useCallback(
-  //   (name: string, value: string) => {
-  //     const params = new URLSearchParams(searchParams.toString())
-  //     params.set(name, value)
-
-  //     return params.toString()
-  //   },
-  //   [searchParams],
-  // )
-
   return (
     <div className='grid grid-cols-2 gap-8 lg:grid-cols-3 2xl:grid-cols-4'>
       {jobs.map((job) => (
-        <JobsListItem
-          key={job.id}
-          id={job.id}
-          title={job.title}
-          description={job.description}
-          company={job.company}
-        />
+        <DialogTrigger key={job.id}>
+          <JobsListItem
+            id={job.id}
+            title={job.title}
+            description={job.description}
+            company={job.company}
+          />
+        </DialogTrigger>
       ))}
     </div>
   )
