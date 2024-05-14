3a4e704e5fc6a41b883588f5e152f5d98e205f36
Pete <pete@manageable.works>
2024-05-14 14:59:47 +0100

    feat: add x-pathname header to request in middleware

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-pathname', request.nextUrl.pathname)
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+}
+
+// See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: '/about/:path*',
+// }
