d2cb32e3087aedadb9392cd8ab6f4a12394ca9af
Pete <pete@manageable.works>
2024-05-21 22:56:50 +0100

    refactor: remove redundant loaders

--- src/app/(auth)/talent/jobs/loaders.ts
+++ src/app/(auth)/talent/jobs/loaders.ts
@@ -2,7 +2,7 @@
 import qs from 'qs'
 
 import { env } from '@/config/env'
-import { getToken } from '@/lib/auth/token'
+import { getAuthToken } from '@/lib/auth/token'
 
 import { Job } from './page'
 
@@ -12,35 +12,11 @@ type PopulateParams =
     }
   | Record<string, never>
 
-type FiltersParams =
-  | {
-      [key: string]: string | number | boolean
-    }
-  | Record<string, never>
-
-type Params = { populate?: PopulateParams } & { filters?: FiltersParams }
-
-export async function getJobs(params: Params = {}): Promise<Job[]> {
-  const token = getToken()
-  const baseUrl = `${env.SERVER_URL}/api/v1/jobs`
-  const query = params ? `?${qs.stringify(params)}` : ''
-  const url = `${baseUrl}${query}`
-
-  const jobsRes = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-
-  const jobsResBody = await jobsRes.json()
-  return jobsResBody.data
-}
-
 export async function getJob(
   id: number,
   populate: PopulateParams = {},
 ): Promise<Job> {
-  const token = getToken()
+  const token = getAuthToken()
   const baseUrl = `${env.SERVER_URL}/api/v1/jobs/${id}`
   const query = populate ? `?${qs.stringify({ populate })}` : ''
   const url = `${baseUrl}${query}`
