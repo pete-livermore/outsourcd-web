48a2091a1cf33f67ab8320640f3f55f3b90115a6
Pete Livermore <peterlivermore@gmail.com>
2024-06-13 14:20:57 +1000

    feat: create new not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import { ArrowRight, Hammer } from 'lucide-react'
+import Link from 'next/link'
+
+import { Heading } from '@/components/ui/heading'
+
+export default function NotFoundPage() {
+  return (
+    <div className='flex h-screen w-full items-center justify-center px-4'>
+      <div className='flex flex-col justify-center space-y-3'>
+        <div>
+          <Heading level={1} size='xl'>
+            <Hammer className='mr-2 inline' size={48} />
+            This page is under construction
+          </Heading>
+          <p className='text-xl'>Please check back later</p>
+        </div>
+        <div className='flex justify-end'>
+          <Link href='/' className='text-lg text-primary underline'>
+            Go home <ArrowRight className='inline' size={20} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
