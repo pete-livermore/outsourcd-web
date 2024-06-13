d7736b2e4975282bb45102b6b211b181783ea315
Pete Livermore <peterlivermore@gmail.com>
2024-06-13 14:19:50 +1000

    refactor: separate size and weight variants of heading component

--- src/components/ui/heading/heading.tsx
+++ src/components/ui/heading/heading.tsx
@@ -12,12 +12,13 @@ export interface HeadingProps
 const headingVariants = cva([], {
   variants: {
     size: {
-      xxl: 'my-8 text-6xl font-bold sm:mb-4 md:mt-4',
-      xl: 'my-2 text-4xl font-bold sm:mb-4 md:mt-4',
-      lg: 'my-6 text-2xl font-semibold sm:mb-3 md:mt-3',
-      md: 'my-7 text-xl sm:mb-3 md:mt-3',
-      sm: 'my-7 text-lg sm:mb-3 md:mt-3',
+      xxl: 'my-8 text-6xl sm:mb-4 md:mt-4',
+      xl: 'my-2 text-5xl sm:mb-4 md:mt-4',
+      lg: 'my-6 text-4xl sm:mb-3 md:mt-3',
+      md: 'my-7 text-3xl sm:mb-3 md:mt-3',
+      sm: 'my-7 text-2xl sm:mb-3 md:mt-3',
     },
+    weight: { bold: 'font-bold', semibold: 'font-semibold' },
     textColor: {
       primary: 'text-primary',
       grey: 'text-gray-600',
@@ -30,6 +31,7 @@ const headingVariants = cva([], {
   },
   defaultVariants: {
     size: 'lg',
+    weight: 'bold',
     textColor: 'primary',
     justification: 'center',
   },
@@ -43,7 +45,6 @@ export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
       level = 'h1',
       size = 'lg',
       textColor,
-      children,
       ...props
     },
     ref,
@@ -56,25 +57,13 @@ export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
 
     switch (level) {
       case 1: {
-        return (
-          <h1 {...elementProps} ref={ref}>
-            {children}
-          </h1>
-        )
+        return <h1 ref={ref} {...elementProps}></h1>
       }
       case 2: {
-        return (
-          <h2 {...elementProps} ref={ref}>
-            {children}
-          </h2>
-        )
+        return <h2 ref={ref} {...elementProps}></h2>
       }
       case 3: {
-        return (
-          <h3 {...elementProps} ref={ref}>
-            {children}
-          </h3>
-        )
+        return <h3 ref={ref} {...elementProps}></h3>
       }
       default:
         return null
