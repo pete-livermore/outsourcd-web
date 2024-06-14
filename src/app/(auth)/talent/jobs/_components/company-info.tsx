9f190d397168cd8c107485cd3391db7dc356e708
Pete Livermore <peterlivermore@gmail.com>
2024-06-14 20:51:05 +1000

    refactor: change text size

--- src/app/(auth)/talent/jobs/_components/company-info.tsx
+++ src/app/(auth)/talent/jobs/_components/company-info.tsx
@@ -6,7 +6,7 @@ export function CompanyInfo({ name }: { name: string }) {
   return (
     <div className='flex items-center'>
       <CldImage src='pc3ln9fb8myr9t5b5apw' alt={name} width={60} height={60} />
-      <p className='text-lg font-semibold'>{name}</p>
+      <p className='text-base font-semibold'>{name}</p>
     </div>
   )
 }
