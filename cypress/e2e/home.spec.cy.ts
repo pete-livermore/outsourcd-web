cc56f2846a74975e491fa749831f25219bf5f82a
Pete Livermore <peterlivermore@gmail.com>
2024-06-15 15:44:05 +1000

    test: add baseUrl to cypress config

--- cypress/e2e/home.spec.cy.ts
+++ cypress/e2e/home.spec.cy.ts
@@ -1,6 +1,6 @@
 describe('homepage', () => {
   it('displays the hero section, heading text and CTA link', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
     cy.get('[data-testid="homepage-hero"]').should('exist')
     cy.get('h1').contains('Find a job that works for you')
     cy.get('a').contains('Get started')
