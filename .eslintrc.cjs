e1a16dfce6e5d79146343f860655900d5f89edfe
Pete Livermore <peterlivermore@gmail.com>
2024-06-15 15:06:57 +1000

    feat: add storybook for component testing and documentation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:storybook/recommended',
-    'plugin:tailwindcss/recommended',
-  ],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended', 'plugin:tailwindcss/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'simple-import-sort'],
   root: true,
