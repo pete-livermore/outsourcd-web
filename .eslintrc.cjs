e55a2ced19390112dd0aac1d8cf5db4e64f8395c
Pete Livermore <peterlivermore@gmail.com>
2024-06-15 15:24:30 +1000

    style: whitelist custom class

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,23 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended', 'plugin:tailwindcss/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'simple-import-sort'],
   root: true,
   rules: {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    'tailwindcss/no-custom-classname': [
+      1,
+      {
+        whitelist: ['destructive'],
+      },
+    ],
   },
 }
