50b70127b0827eab19faf3580975edfd75b0e8ac
Pete Livermore <peterlivermore@gmail.com>
2024-06-29 15:31:23 +1000

    refactor: fetch and render dynamic page data

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,24 +2,32 @@ import Link from 'next/link'
 
 import { buttonVariants } from '@/components/ui/button'
 import { Heading } from '@/components/ui/heading'
-import { buildCloudinaryUrl } from '@/lib/images/cloudinary'
+import { prisma } from '@/db/client'
+import { PageSection } from '@/types/entities/page-section'
+import { logger } from '@/utils/logging/logger'
 import { cn } from '@/utils/styles'
 
 import { CompanyLogoCarousel } from './_components/company-logo-carousel'
 
-const HERO_VIDEO_PATH = 'freelance_worker_video.mp4'
+interface HeroSectionProps {
+  data: Omit<PageSection, 'name'>
+}
 
-export function HeroSection() {
+export function HeroSection({ data }: HeroSectionProps) {
+  const { content } = data
+  const videoContent = content.find((cont) => cont.type === 'video')
   return (
-    <section className=''>
+    <section>
       <div className='relative h-screen w-full' data-testid='homepage-hero'>
-        <video
-          src={buildCloudinaryUrl(HERO_VIDEO_PATH, { media: 'video' })}
-          className='size-full object-cover brightness-[40%]'
-          autoPlay
-          muted
-          loop
-        />
+        {videoContent?.url && (
+          <video
+            src={videoContent.url}
+            className='size-full object-cover brightness-[40%]'
+            autoPlay
+            muted
+            loop
+          />
+        )}
         <div className='absolute inset-1/2 flex h-fit min-w-[600px] -translate-x-1/2 -translate-y-1/2 flex-col items-center space-y-8 xl:items-start'>
           <Heading
             as='h1'
@@ -49,10 +57,30 @@ export function HeroSection() {
   )
 }
 
-export default function HomePage() {
+interface Sections {
+  [key: string]: Omit<PageSection, 'name'>
+}
+
+export default async function HomePage() {
+  const pageData = await prisma.page.findUnique({
+    where: { slug: '/' },
+    include: { sections: { include: { content: true } } },
+  })
+
+  if (!pageData) {
+    logger.error('error retrieving homepage data from DB')
+    throw new Error()
+  }
+
+  const sections = pageData.sections.reduce((acc: Sections, sec) => {
+    const { name, ...rest } = sec
+    acc[name] = rest
+    return acc
+  }, {})
+
   return (
     <div>
-      <HeroSection />
+      {sections.hero && <HeroSection data={sections.hero} />}
       <section className='bg-muted'>
         <div className='py-6'>
           <CompanyLogoCarousel />
