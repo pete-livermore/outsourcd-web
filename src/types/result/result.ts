868ff664752044a7e13839152b34a664fb59b621
Pete Livermore <peterlivermore@gmail.com>
2024-07-12 23:04:21 +1000

    feat: create result type

---
+++ src/types/result/result.ts
@@ -0,0 +1,20 @@
+export type ResultType = 'success' | 'failure'
+
+type FailureReason = 'auth-error' | 'server-error'
+
+interface BaseResult {
+  type: ResultType
+}
+
+interface SuccessResult<T> extends BaseResult {
+  type: 'success'
+  data: T extends void ? undefined : T
+}
+
+interface FailureResult extends BaseResult {
+  type: 'failure'
+  reason: FailureReason
+  errors?: string[]
+}
+
+export type Result<T = void> = SuccessResult<T> | FailureResult
