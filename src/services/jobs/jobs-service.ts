7db92593a76169c17332f4db70d9e212168d58ca
Pete Livermore <peterlivermore@gmail.com>
2024-07-12 23:06:01 +1000

    refactor: add logic to parse Dto

--- src/services/jobs/jobs-service.ts
+++ src/services/jobs/jobs-service.ts
@@ -1,7 +1,9 @@
 import qs from 'qs'
 
-import { ApiClient } from '@/lib/api/api-client'
-import { Job, JobData } from '@/models/job'
+import { HTTPError } from '@/errors/http-error'
+import { IApiClient } from '@/lib/api/client/api-client'
+import { Result } from '@/types/result/result'
+import { logger } from '@/utils/logging/logger'
 
 type PopulateParams =
   | {
@@ -9,41 +11,115 @@ type PopulateParams =
     }
   | Record<string, never>
 
-type FilterParams = Record<string, any>
+type FilterParams = Record<string, unknown>
 
 interface GetJobsParams {
   filters?: FilterParams
   populate?: PopulateParams
 }
 
+interface CompanyRelationDto {
+  id: number
+  name: string
+}
+
+interface SalaryDto {
+  currency: string
+  value: { max: number; min: number }
+  period: string
+}
+
+export interface JobDto {
+  id: number
+  title: string
+  description: string
+  company: CompanyRelationDto
+  salary: SalaryDto
+  employment_type: string
+  start_date: string
+}
+
+export interface Job {
+  id: number
+  title: string
+  description: string
+  company: CompanyRelationDto
+  salary: SalaryDto
+  employmentType: string
+  startDate: string
+}
+
 export class JobsService {
-  constructor(private readonly apiClient: ApiClient) {
+  constructor(private readonly apiClient: IApiClient) {
     this.apiClient = apiClient
   }
 
   private readonly jobsApiPath = '/api/v1/jobs'
 
-  async getOne(id: number, populate: PopulateParams | undefined = {}) {
+  private parseDto(dto: JobDto) {
+    return {
+      id: dto.id,
+      title: dto.title,
+      description: dto.description,
+      company: dto.company,
+      salary: dto.salary,
+      employmentType: dto.employment_type,
+      startDate: dto.start_date,
+    }
+  }
+
+  async getOne(
+    id: number,
+    populate: PopulateParams | undefined = {},
+  ): Promise<Result<Job>> {
     const basePath = `${this.jobsApiPath}/${id}`
     const query = qs.stringify({ populate })
     const apiPath = basePath + (query ? `?${query}` : '')
-    const result = await this.apiClient.get<JobData>(apiPath)
 
-    if (result.type === 'success') {
-      return { type: result.type, data: new Job(result.data) }
+    try {
+      const data = await this.apiClient.get<JobDto>(apiPath)
+      return {
+        type: 'success',
+        data: this.parseDto(data),
+      }
+    } catch (e) {
+      logger.error(e)
+      if (e instanceof HTTPError && e.status === 401) {
+        return {
+          type: 'failure',
+          reason: 'auth-error',
+        }
+      }
+      return {
+        type: 'failure',
+        reason: 'server-error',
+      }
     }
-    return result
   }
 
-  async getMany(params?: GetJobsParams) {
-    const { filters = {}, populate = {} } = params || {}
+  async getMany(params?: GetJobsParams): Promise<Result<Job[]>> {
+    const { filters, populate } = params || {}
     const query = qs.stringify({ filters, populate })
     const apiPath = `${this.jobsApiPath}?${query}`
-    const result = await this.apiClient.get<JobData[]>(apiPath)
 
-    if (result.type === 'success') {
-      return { type: result.type, data: result.data.map((job) => new Job(job)) }
+    try {
+      const data = await this.apiClient.get<JobDto[]>(apiPath)
+      return {
+        type: 'success',
+        data: data.map((job) => this.parseDto(job)),
+      }
+    } catch (e) {
+      logger.error(e)
+      if (e instanceof HTTPError && e.status === 401) {
+        return {
+          type: 'failure',
+          reason: 'auth-error',
+        }
+      }
+      return {
+        type: 'failure',
+        reason: 'server-error',
+      }
     }
-    return result
   }
 }
