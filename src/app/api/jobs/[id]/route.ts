e2a2a3d7c3aed7350073d90403aebb770b86d4c0
Pete Livermore <peterlivermore@gmail.com>
2024-07-13 15:47:45 +1000

    refactor: call new generateErrResponse fn to send JSON response

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -1,36 +1,34 @@
-import { jobsService } from '@/services/jobs'
+import { apiClient } from '@/lib/api/client'
+import { getAuthToken } from '@/lib/auth/token'
+import { generateErrResponse } from '@/lib/response/generate-error-response'
+import { JobsService } from '@/services/jobs/jobs-service'
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
 ) {
+  const token = getAuthToken()
+
+  if (!token) {
+    return generateErrResponse('unauthenticated')
+  }
+
   const { id } = params
 
   if (!id) {
     return
   }
 
+  apiClient.authenticate(token)
+  const jobsService = new JobsService(apiClient)
+
   const jobResult = await jobsService.getOne(parseInt(id), { company: true })
 
   if (jobResult.type === 'failure') {
-    if (jobResult.failureReason === 'auth-error') {
-      return Response.json(
-        {
-          message: 'Unauthenticated',
-        },
-        {
-          status: 401,
-        },
-      )
+    if (jobResult.reason === 'auth-error') {
+      return generateErrResponse('unauthenticated')
     }
-    return Response.json(
-      {
-        message: 'Internal Server Error',
-      },
-      {
-        status: 500,
-      },
-    )
+    return generateErrResponse('server')
   }
 
   const { data: job } = jobResult
