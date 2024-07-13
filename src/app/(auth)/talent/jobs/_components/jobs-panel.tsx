258d086df8062129ebec1907d6407c941dd9aa30
Pete Livermore <peterlivermore@gmail.com>
2024-07-13 16:10:38 +1000

    fix: import correct Job interface

--- src/app/(auth)/talent/jobs/_components/jobs-panel.tsx
+++ src/app/(auth)/talent/jobs/_components/jobs-panel.tsx
@@ -10,7 +10,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
-import { Job } from '@/models/job'
+import { Job } from '@/services/jobs/jobs-service'
 
 import { JobDetail } from './job-detail'
 import { JobFilters } from './job-filters'
