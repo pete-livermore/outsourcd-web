444861d2e9847e7db37cd1ba24f702bf4e14e25a
Pete Livermore <peterlivermore@gmail.com>
2024-07-16 19:16:51 +1000

    feat: add LOG LEVEL as env variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,20 +2,22 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   AUTH_COOKIE_NAME: z.string(),
-  DOMAIN: z.string(),
-  SERVER_URL: z.string().url(),
   CLOUDINARY_CLOUD_NAME: z.string(),
   CLOUDINARY_API_KEY: z.string(),
   CLOUDINARY_API_SECRET: z.string(),
   CLOUDINARY_FOLDER_NAME: z.string(),
+  DOMAIN: z.string(),
+  LOG_LEVEL: z.string().optional(),
+  SERVER_URL: z.string().url(),
 })
 
 export const env = envSchema.parse({
   AUTH_COOKIE_NAME: process.env.AUTH_COOKIE_NAME || '_auth',
-  DOMAIN: process.env.DOMAIN || 'localhost',
-  SERVER_URL: process.env.SERVER_URL,
   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
   CLOUDINARY_FOLDER_NAME: process.env.CLOUDINARY_FOLDER_NAME,
+  DOMAIN: process.env.DOMAIN || 'localhost',
+  LOG_LEVEL: process.env.LOG_LEVEL,
+  SERVER_URL: process.env.SERVER_URL,
 })
--- src/utils/logging/logger.ts
+++ src/utils/logging/logger.ts
@@ -1,19 +1,35 @@
 import winston from 'winston'
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
+import { env } from '@/config/env'
+
+const { combine, timestamp, printf, colorize, json } = winston.format
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`
 })
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
+const logger = winston.createLogger({
+  level: env.LOG_LEVEL || 'info',
+  format: combine(
+    colorize({ all: true }),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
     }),
+    logFormat,
+  ),
+  transports: [new winston.transports.Console()],
+})
+
+if (process.env.NODE_ENV === 'production') {
+  logger.add(
+    new winston.transports.File({
+      filename: 'error.log',
+      level: 'error',
+      format: json(),
+    }),
+  )
+  logger.add(
+    new winston.transports.File({ filename: 'combined.log', format: json() }),
   )
 }
 
