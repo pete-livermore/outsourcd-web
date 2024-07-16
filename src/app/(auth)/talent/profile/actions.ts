280dfae50385bd0d2d9e2fdc3f145e9ae3ded209
Pete Livermore <peterlivermore@gmail.com>
2024-07-16 16:20:57 +1000

    refactor: abstract data fetching logic into services

--- src/app/(auth)/talent/profile/actions.ts
+++ src/app/(auth)/talent/profile/actions.ts
@@ -1,54 +1,27 @@
 'use server'
 
-import * as changeCase from 'change-case'
 import { fileTypeFromBlob } from 'file-type'
+import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
-import { env } from '@/config/env'
+import { VALIDATION_ERROR_MESSAGE } from '@/constants/errors/messages/validation-error-message'
+import { ApiClient } from '@/lib/api/client/api-client'
 import { getAuthToken } from '@/lib/auth/token'
-import { userService } from '@/services/user'
-import { UpdateUserDto } from '@/services/user/user-service'
-import { logger } from '@/utils/logging/logger'
+import { authRedirect } from '@/lib/navigation/redirect'
+import { MediaService } from '@/services/media/media-service'
+import { UsersService } from '@/services/users/users-service'
 
 import { ProfileFormState } from './_components/profile-form'
 
-async function uploadUserProfileImage(blob: Blob, userId: number) {
-  const token = getAuthToken()
-  const fileName = `profile_image_${userId}`
-  const fileType = await fileTypeFromBlob(blob)
-  const fileExt = fileType?.ext
-  const fileOriginalName = fileName + '.' + fileExt
-
-  const newForm = new FormData()
-  newForm.append('file', blob, fileOriginalName)
-  newForm.append('provider', 'cloudinary')
-  newForm.append('name', fileName)
-
-  const res = await fetch(`${env.SERVER_URL}/api/v1/uploads/file`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    method: 'post',
-    body: newForm,
-  })
-
-  if (!res.ok) {
-    logger.error(
-      `image ${fileName} not uploaded (received status ${res.status})`,
-    )
-  }
-
-  const resBody = await res.json()
-  return resBody.data
-}
+const SERVER_ERROR_MESSAGE = 'Sorry, there was a server error'
 
 const updateUserSchema = z.object({
   // email: z.string({
   //   invalid_type_error: 'Invalid Email',
   // }),
-  firstName: z.string().nullable(),
-  lastName: z.string().nullable(),
-  biography: z.string().nullable(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  biography: z.string().optional(),
   profileImage: z.number().gte(1).optional(),
 })
 
@@ -61,59 +34,78 @@ interface RawFormData {
 
 export async function updateUser(
   additionalFormData: FormData,
-  _prevState: ProfileFormState,
+  _prevState: ProfileFormState | null,
   formData: FormData,
 ): Promise<ProfileFormState> {
-  const result = await userService.getAuthenticatedUser()
+  const token = getAuthToken()
 
-  if (result.type === 'failure') {
-    return { result: 'failure' }
+  if (!token) {
+    authRedirect()
   }
 
-  const user = result.data
+  const usersService = UsersService.getInstance(ApiClient.getInstance(token))
+  const getUserResult = await usersService.getAuthenticatedUser()
 
-  const profileImage = additionalFormData.get('profileImage') as Blob
+  if (getUserResult.type === 'failure') {
+    return authRedirect()
+  }
+
+  const user = getUserResult.data
 
   const rawFormData: RawFormData = {
     biography: formData.get('biography'),
-    firstName: formData.get('first-name'),
-    lastName: formData.get('last-name'),
+    firstName: formData.get('firstname'),
+    lastName: formData.get('lastname'),
   }
 
+  const profileImage = additionalFormData.get('profileImage') as Blob
+
   if (profileImage) {
-    const newImage = await uploadUserProfileImage(profileImage, user.id)
-    rawFormData.profileImage = newImage.id
+    const fileName = `profile_image_${user.id}`
+    const fileType = await fileTypeFromBlob(profileImage)
+
+    if (!fileType) {
+      return {
+        type: 'failure',
+        message: VALIDATION_ERROR_MESSAGE,
+        errors: { profileImage: ['Profile image must be a valid file type'] },
+      }
+    }
+
+    const mediaService = MediaService.getInstance(ApiClient.getInstance(token))
+    const uploadResult = await mediaService.upload(profileImage, {
+      ext: fileType.ext,
+      name: fileName,
+    })
+
+    if (uploadResult.type === 'failure') {
+      if (uploadResult.reason === 'auth-error') {
+        redirect('/auth/login')
+      }
+      return { type: 'failure', message: SERVER_ERROR_MESSAGE }
+    }
+
+    rawFormData.profileImage = uploadResult.data.id
   }
 
   const validatedFields = updateUserSchema.safeParse(rawFormData)
 
   if (!validatedFields.success) {
     return {
+      type: 'failure',
+      message: VALIDATION_ERROR_MESSAGE,
       errors: validatedFields.error.flatten().fieldErrors,
     }
   }
 
-  // eslint-disable-next-line
-  const updateUserDto: Record<string, any> = {}
-
-  for (const [key, value] of Object.entries(rawFormData)) {
-    if (value !== undefined && value !== null && value !== '') {
-      const snakeCaseKey = changeCase.snakeCase(key)
-      updateUserDto[snakeCaseKey] = value
-    }
-  }
-
-  const updateResult = await userService.update(
-    user.id,
-    updateUserDto as UpdateUserDto,
-  )
+  const updateResult = await usersService.update(user.id, validatedFields.data)
 
   if (updateResult.type === 'failure') {
-    const failureReason =
-      updateResult.reason === 'auth-error' ? 'authentication' : 'server'
-    logger.error(`user ${user.id} not updated due to ${failureReason} error`)
-    return { result: 'failure' }
+    if (updateResult.reason === 'auth-error') {
+      redirect('/auth/login')
+    }
+    return { type: 'failure', message: SERVER_ERROR_MESSAGE }
   }
 
-  return { result: 'success' }
+  return { type: 'success', message: 'User updated succesfully' }
 }
