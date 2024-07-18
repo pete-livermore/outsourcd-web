6cf87c73b7891fa9c1f6d2b02434a0016169818e
Pete Livermore <peterlivermore@gmail.com>
2024-07-18 16:38:47 +1000

    test: add new test case for homepage

--- cypress/e2e/home.spec.cy.ts
+++ cypress/e2e/home.spec.cy.ts
@@ -2,7 +2,16 @@ describe('homepage', () => {
   it('displays the hero section, heading text and CTA link', () => {
     cy.visit('/')
     cy.get('[data-testid="homepage-hero"]').should('exist')
-    cy.get('h1').contains('Find a job that works for you')
-    cy.get('a').contains('Get started')
+    cy.get('h1').contains('Find a job that works for you').should('exist')
+    cy.get('a').contains('Get started').should('exist')
+  })
+
+  it('navigates to the login page (with redirect param) when the CTA button is clicked', () => {
+    cy.visit('/')
+    cy.get('a').contains('Get started').click()
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/auth/login')
+      expect(loc.search).contains('?redirect=')
+    })
   })
 })
